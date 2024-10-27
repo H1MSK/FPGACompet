ac106a508d16e27cfe6f32162015da1ba0ee7336
H1MSK <ksda47832338@outlook.com>
2024-10-27 17:29:42 +0800

    impl: sim for AxiStreamWidthAdaptors

---
+++ hw/spinal/adaptors/AxiStreamWidthAdaptorI8O32Sim.scala
@@ -0,0 +1,35 @@
+package adaptors
+
+package axi_lite_regs
+
+import scala.collection.mutable
+
+import spinal.core._
+import spinal.core.sim._
+import spinal.lib.bus.amba4.axis.sim.{Axi4StreamMaster, Axi4StreamSlave}
+
+object AxiStreamWidthAdaptorI8O32Sim extends App {
+  ProjectConfig.sim.compile(AxiStreamWidthAdaptorI8O32()).doSim { dut =>
+    val stream_master = Axi4StreamMaster(dut.io.i, dut.clockDomain)
+    val stream_slave = Axi4StreamSlave(dut.io.o, dut.clockDomain)
+    dut.clockDomain.forkStimulus(period = 10)
+    dut.clockDomain.forkSimSpeedPrinter()
+    dut.clockDomain.assertReset()
+
+    waitUntil(dut.clockDomain.resetSim.toBoolean != (dut.clockDomain.config.resetActiveLevel == HIGH))
+
+    (0 until 32).foreach { _ =>
+      val data = List.fill((1 + simRandom.nextInt(15)) * 4)(simRandom.nextInt(256).toByte)
+      val thread_send = fork {
+        stream_master.send(data)
+      }
+      var recv: List[Byte] = null
+      val thread_recv = fork {
+        recv = stream_slave.recv()
+      }
+      thread_recv.join()
+      thread_send.join()
+      assert(recv.toList sameElements data)
+    }
+  }
+}
