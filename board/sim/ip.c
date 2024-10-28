b51c4413ee85b95bd53d81966f07e6a97478dda0
H1MSK <ksda47832338@outlook.com>
2024-10-28 19:35:29 +0800

    fix(sim): add sim memory for IP_get/set

--- board/sim/ip.c
+++ board/sim/ip.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 #include "pal.h"
+
+uint32_t sim_mem[1024];
 
 void IP_init() {
 }
@@ -11,6 +14,8 @@ void IP_process(uint16_t width, uint16_t height, const uint8_t* in, uint8_t* out
 }
 
 void IP_set(uint32_t addr, uint32_t val) {
+  assert(addr < 1024);
+  sim_mem[addr] = val;
   printf("IP_set@%d:%d.\n", addr, val);
   if (addr == 0 && val == 0x01) {
     IP_process(1024, 1024, in_img, out_img);
@@ -18,7 +23,8 @@ void IP_set(uint32_t addr, uint32_t val) {
 }
 
 uint32_t IP_get(uint32_t addr) {
+  assert(addr < 1024);
   printf("IP_get@%d.\n", addr);
   if (addr == 1) return 0x01;
-  return 0;
+  return sim_mem[addr];
 }
