76e5d5418c018e3679748573850d7a6d3c35b79e
H1MSK <ksda47832338@outlook.com>
2024-11-06 00:35:30 +0800

    update: use BipedDataset

--- pymodel/model.py
+++ pymodel/model.py
@@ -3,7 +3,7 @@ from torch import nn
 class Model(nn.Module):
   def __init__(self, hidden_channels: int):
     super(Model, self).__init__()
-    self.conv1 = nn.Conv2d(1, hidden_channels, 3, 1, padding=1, bias=False)
+    self.conv1 = nn.Conv2d(3, hidden_channels, 3, 1, padding=1, bias=False)
     self.conv2 = nn.Conv2d(hidden_channels, 1, 3, 1, padding=1, bias=False)
     
   def forward(self, x):
--- pymodel/train.py
+++ pymodel/train.py
@@ -3,10 +3,10 @@ import torch
 from torch import nn
 from torch import functional as F
 from lightning.pytorch.callbacks import ModelCheckpoint
+from torch.utils.tensorboard import SummaryWriter
 
-from dataset import MyDataSet
+from dataset import BipedDataset
 from model import Model
-from lane_dataset import LaneDataSet
 
 class LitModel(L.LightningModule):
   def __init__(self, hidden_channels):
@@ -27,7 +27,7 @@ class LitModel(L.LightningModule):
     pred = self.model(x)
     loss = self.loss_fn(pred, y)
     self.log("val_loss", loss)
-    tensorboard = self.logger.experiment
+    tensorboard: SummaryWriter = self.logger.experiment
     tensorboard.add_image("input", x[0], self.global_step)
     tensorboard.add_image("prediction", pred[0], self.global_step)
     tensorboard.add_image("ground_truth", y[0], self.global_step)
@@ -38,14 +38,17 @@ class LitModel(L.LightningModule):
     return {"optimizer": self.opt, "lr_scheduler": self.sched, "monitor": "train_loss"}
   
 train_model = LitModel(32)
-data_loader = torch.utils.data.DataLoader(LaneDataSet(train=True), batch_size=32)
-val_data_loader = torch.utils.data.DataLoader(LaneDataSet(train=False), batch_size=32)
+dataset = BipedDataset("D:/DataSets/BIPED/", 720, 1080, 0)
+train_dataset, val_dataset = torch.utils.data.random_split(dataset, [0.9, 0.1])
+data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16)
+val_data_loader = torch.utils.data.DataLoader(val_dataset, batch_size=16)
 
 trainer = L.Trainer(
-  max_epochs=10,
+  max_epochs=-1,
   default_root_dir="./train",
+  log_every_n_steps=2,
   callbacks=[
     ModelCheckpoint(save_last=True, every_n_epochs=1)
   ]
-  )
+)
 trainer.fit(train_model, data_loader, val_data_loader)
