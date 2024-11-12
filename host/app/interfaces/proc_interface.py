454aa36d733e80c735fd36b3b6aedc90b2e3f7d9
H1MSK <ksda47832338@outlook.com>
2024-11-12 16:12:39 +0800

    tweak: multiple changes
    fix: pick image with size=(1024,1024) now successfully applies
    fix: test_in and test_out now resizes to 1024x1024
    change: output image now scales from (TH_L,TH_H) to (0, 255)

--- host/app/interfaces/proc_interface.py
+++ host/app/interfaces/proc_interface.py
@@ -19,7 +19,7 @@ import web_client
 class ProcInterface(QFrame):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
-        
+
         screen_geometry = QGuiApplication.primaryScreen().availableGeometry()
         print(
             "screen_size = "
@@ -51,12 +51,14 @@ class ProcInterface(QFrame):
         self.grid_layout.addWidget(self.threshold_low_card, 2, 0)
         self.grid_layout.addWidget(self.threshold_high_card, 2, 1)
 
-        self.input_image = ImageLabel(self)
-        self.input_image.clicked.connect(self.pickInputImage)
-        self.output_image = ImageLabel(self)
-        self.output_image.clicked.connect(self.saveOutputImage)
-        self.grid_layout.addWidget(self.input_image, 3, 0)
-        self.grid_layout.addWidget(self.output_image, 3, 1)
+        self.input_image_label = ImageLabel(self)
+        self.input_image_label.clicked.connect(self.pickInputImage)
+        self.output_image_dat: bytes = bytes()
+        self.output_image: QImage = QImage()
+        self.output_image_label = ImageLabel(self)
+        self.output_image_label.clicked.connect(self.saveOutputImage)
+        self.grid_layout.addWidget(self.input_image_label, 3, 0)
+        self.grid_layout.addWidget(self.output_image_label, 3, 1)
         self.grid_layout.setRowStretch(3, 1)
 
         self.run_button = PrimaryPushButton(FIF.PLAY, "运行", self)
@@ -73,29 +75,41 @@ class ProcInterface(QFrame):
         QTimer.singleShot(0, self.preloadImages)
         QTimer.singleShot(0, self.updateKernelText)
 
+        app_cfg.threshold_high.valueChanged.connect(self.updateOutputImage)
+        app_cfg.threshold_low.valueChanged.connect(self.updateOutputImage)
+
     @Slot()
     def preloadImages(self):
         if os.path.exists("test_in.png"):
-            self.input_image.setImage("test_in.png")
-            self.input_image.scaledToWidth(self.column_width)
+            img = QImage("test_in.png").scaled(1024, 1024)
+            self.input_image_label.setImage(img)
+            self.input_image_label.scaledToWidth(self.column_width)
         if os.path.exists("test_out.png"):
-            self.output_image.setImage("test_out.png")
-            self.output_image.scaledToWidth(self.column_width)
-            
+            self.output_image = (
+                QImage("test_out.png")
+                .scaled(1024, 1024)
+                .toPixelFormat(QImage.Format.Format_Grayscale8)
+            )
+            self.updateOutputImage()
+
     @Slot()
     def pickInputImage(self):
         while True:
-            name, _ = QFileDialog.getOpenFileName(self, "选择输入图像", "", "图像文件 (*.bmp *.jpeg *.jpg *.png)")
+            name, _ = QFileDialog.getOpenFileName(
+                self, "选择输入图像", "", "图像文件 (*.bmp *.jpeg *.jpg *.png)"
+            )
             if len(name) == 0:
                 return
             img = QImage(name, format=None)
             if img.width() != 1024 or img.height() != 1024:
                 from app.main_window import main_window
+
                 w = Dialog(
                     "自动转换",
                     f"选中图像尺寸为{img.width()}x{img.height()}，"
                     "输入图像尺寸需要为1024x1024，是否进行自动转换？",
-                    main_window)
+                    main_window,
+                )
                 w.yesButton.setText("转换")
                 w.cancelButton.setText("重新选择...")
                 if not w.exec():
@@ -104,14 +118,18 @@ class ProcInterface(QFrame):
                 else:
                     img = img.scaled(1024, 1024)
                     break
-        self.input_image.setImage(img)
-        self.input_image.scaledToWidth(self.column_width)
+            else:
+                break
+        self.input_image_label.setImage(img)
+        self.input_image_label.scaledToWidth(self.column_width)
 
     @Slot()
     def saveOutputImage(self):
-        name, _ = QFileDialog.getSaveFileName(self, "保存输出图像", "", "图像文件 (*.bmp *.jpeg *.jpg *.png)")
+        name, _ = QFileDialog.getSaveFileName(
+            self, "保存输出图像", "", "图像文件 (*.bmp *.jpeg *.jpg *.png)"
+        )
         if name:
-            self.output_image.image.save(name)
+            self.output_image_label.image.save(name)
 
     @Slot()
     def updateKernelText(self):
@@ -124,19 +142,42 @@ class ProcInterface(QFrame):
     @Slot()
     def openKernelSetMessageBox(self):
         from app.main_window import main_window
+
         main_window.openKernelSetMessageBox()
 
     @Slot()
     def run(self):
         app_cfg.upload()
-        in_dat = self.input_image.image.convertToFormat(QImage.Format.Format_Grayscale8)
+        in_dat = self.input_image_label.image.convertToFormat(
+            QImage.Format.Format_Grayscale8
+        )
+        with open(os.path.dirname(__file__) + "/model.bin", "rb") as fp:
+            buf = fp.read()
+        web_client.write_weights(buf)
         web_client.write_img(in_dat.constBits())
         web_client.write_arg(0x00, 0x01)
         out_dat = bytes(web_client.read_img())
-        out_img = QImage(out_dat, 1024, 1024, QImage.Format.Format_Grayscale8)
-        self.output_image.setImage(out_img)
-        self.output_image.scaledToWidth(self.column_width)
-        out_img.save("out_img.png")
+        self.output_image_dat = out_dat
+        self.output_image: QImage = QImage(
+            out_dat, 1024, 1024, QImage.Format.Format_Grayscale8
+        )
+        self.output_image.save("test_out.png")
+        self.updateOutputImage()
+
+    @Slot()
+    def updateOutputImage(self):
+        if len(self.output_image_dat) == 0:
+            return
+        low = app_cfg.threshold_low.value
+        high = app_cfg.threshold_high.value
+        ratio = max(1, high - low)
+        ints = list(min(255, max(0, round((pix - low) / ratio * 255))) for pix in self.output_image_dat)
+        dat = bytes(ints)
+        self.output_image = QImage(
+            dat, 1024, 1024, QImage.Format.Format_Grayscale8
+        )
+        self.output_image_label.setImage(self.output_image)
+        self.output_image_label.scaledToWidth(self.column_width)
 
     @Slot()
     def onConnectionStateChanged(self, connected):
